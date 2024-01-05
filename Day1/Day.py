f0968b472579b1c88f84ecb0d216a13eb6962f91
dedane <evanskeema@gmail.com>
2024-01-06 00:12:27 +0300

    first-commit

---
+++ Day1/Day.py
@@ -0,0 +1,7 @@
+print("Welcome to my 2024 Band")
+
+home = input('What is the name of the place you were born? ')
+exercise = input('What is the name of your favourite exercise? ')
+
+print(' Awesome, your band name is '+ home + " " + exercise) 
+

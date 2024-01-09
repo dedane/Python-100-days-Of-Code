eb0a8b6ea1e87986b17dcb5e6fa95093ee728282
dedane <evanskeema@gmail.com>
2024-01-10 01:22:02 +0300

    third-day-learning-conditions

---
+++ Day3/OddOrEven.py
@@ -0,0 +1,9 @@
+print ('Let find our if your number is odd or even')
+
+number =int(input('Enter your number: '))
+         
+if number % 2 == 0 :
+    print('Your number is even')
+else:
+    print('Your number is odd')
+    

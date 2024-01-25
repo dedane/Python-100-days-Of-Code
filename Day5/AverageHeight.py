b73e22fa3a1b1795ff18dc41b229113ee66ff640
dedane <evanskeema@gmail.com>
2024-01-25 16:00:50 +0300

    average-height

---
+++ Day5/AverageHeight.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+score = 0
+for i, student_score in enumerate(student_scores):
+    score += student_score
+print(f'The average height of the students is: {score / len(student_scores)}')
+# 🚨 Don't change the code above 👆
+#  print(f'The average height of all the students, is {sum(student_heights[n]) / len(student_heights[n])}')
+
+
+
+#Write your code below this row 👇
+

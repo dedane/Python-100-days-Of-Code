069b60f33b1c44d2565a5480bda6331040377054
dedane <evanskeema@gmail.com>
2024-01-26 00:03:35 +0300

    student_height-changed

--- Day5/AverageHeight.py
+++ Day5/AverageHeight.py
@@ -1,12 +1,12 @@
 # 🚨 Don't change the code below 👇
 student_height = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-print(student_scores)
+for n in range(0, len(student_height)):
+  student_height[n] = int(student_height[n])
+print(student_height)
 score = 0
-for i, student_score in enumerate(student_scores):
+for i, student_score in enumerate(student_height):
     score += student_score
-print(f'The average height of the students is: {score / len(student_scores)}')
+print(f'The average height of the students is: {score / len(student_height)}')
 # 🚨 Don't change the code above 👆
 #  print(f'The average height of all the students, is {sum(student_heights[n]) / len(student_heights[n])}')
 
--- Day5/PasswordGenerator.py
+++ Day5/PasswordGenerator.py
@@ -9,6 +9,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#Easy Password Generator
 password = ''
 
 for char in range(1, nr_letters+1):
@@ -21,3 +22,5 @@ for char in range(1, nr_numbers +1):
     password += random.choice(numbers)
 
 print(f'your password is: {password}')
+
+#Hard Password Generator

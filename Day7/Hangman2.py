1e70546663ab38113f04a2f445ca6362029c2be5
dedane <evanskeema@gmail.com>
2024-04-20 10:43:30 +0300

    checking-if-letter-is-available-in-words

--- Day7/Hangman2.py
+++ Day7/Hangman2.py
@@ -11,21 +11,34 @@ print(f'Pssst, the solution is {chosen_word}.')
 display = []
 #For each letter in the chosen_word, add a "_" to 'display'.
 for _ in range(len(chosen_word)):
-    display += '-'
+    display += '_'
 
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-guess = input("Guess a letter: ").lower()
+
+
+end_game  = False
+while not end_game:
+    
+    guess = input("Guess a letter: ").lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range(len(chosen_word)):
-    if guess == chosen_word[position]:
+    for position in range(len(chosen_word)):
         replaced = chosen_word[position]
-        display[position] = replaced
-
-
+        if guess == chosen_word[position]: 
+            display[position] = replaced
+        
+       
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+    print(display)
+    
+    if '_' not in display:
+        print('You win.')
+        end_game = True
+    
+
+        
+            

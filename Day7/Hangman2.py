079b42aeccf50063d44e2a9bcdbdb9cd50d59bda
dedane <evanskeema@gmail.com>
2024-06-09 16:42:03 +0300

    arguments&parameters

--- Day7/Hangman2.py
+++ Day7/Hangman2.py
@@ -1,44 +1,56 @@
-#Step 2
+#Step 4
 
 import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
 
+import Hangman_art
+import Hangman_word
+
+
+
+end_of_game = False
+
+chosen_word = random.choice(Hangman_word.word_list)
+word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6
+print(Hangman_art.logo)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
+#Create blanks
 display = []
-#For each letter in the chosen_word, add a "_" to 'display'.
-for _ in range(len(chosen_word)):
-    display += '_'
+for _ in range(word_length):
+    display += "_"
 
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-
-
-end_game  = False
-while not end_game:
-    
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-    for position in range(len(chosen_word)):
-        replaced = chosen_word[position]
-        if guess == chosen_word[position]: 
-            display[position] = replaced
-        
-       
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-    print(display)
-    
-    if '_' not in display:
-        print('You win.')
-        end_game = True
-    
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-        
-            
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        print(lives)
+        if lives == 0 :
+          end_of_game = True
+          print('you loose')
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(Hangman_art.stages[lives])
---
+++ Day8/greet.py
@@ -0,0 +1,6 @@
+def greet(name): #pass function with parameter
+    print(f'Hello {name}')
+    print('How are you ')
+    print('nice to meet you ')
+
+greet('cynthia') #Adding an argument to be included in the functions output

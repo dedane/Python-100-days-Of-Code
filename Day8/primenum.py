14a2fee36302392e8dbed9b83cfc2065200c5696
dedane <evanskeema@gmail.com>
2024-06-13 06:03:45 +0300

    ceaser-cipher

---
+++ Day8/primenum.py
@@ -0,0 +1,13 @@
+
+def prime_checker(number):
+    if number % 2 == 0:
+        print('This is not a prime number')
+    else:
+        print('This is a prime number')
+
+
+
+
+
+n = int(input('Check this number: '))
+prime_checker(number=n)

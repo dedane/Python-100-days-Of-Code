73818c2c327ded16c5e4f42b2d83b65441450f13
dedane <evanskeema@gmail.com>
2024-06-18 06:25:05 +0300

    ceaser-cipher-encryption

--- Day8/Ceasar_cipher.py
+++ Day8/Ceasar_cipher.py
@@ -5,6 +5,16 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+
+    indices = [alphabet.index(letter) + 1 + shift for letter in text if letter in alphabet]
+    
+    letters = ''.join(alphabet[index -1] for index in indices)
+    
+
+    print(letters)
+
+
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -19,3 +29,4 @@ shift = int(input("Type the shift number:\n"))
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(text,shift)

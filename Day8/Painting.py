49e381c7b1a4c3931abccf079c372689b4177ec9
dedane <evanskeema@gmail.com>
2024-08-28 23:19:02 +0300

    painting-wall

---
+++ Day8/Painting.py
@@ -0,0 +1,18 @@
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+def paint_calc(height,width,cover):
+    print(round((height*width)/coverage))
+
+
+
+#Write your code above this line 👆
+    
+
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
--- Day8/greet.py
+++ Day8/greet.py
@@ -1,6 +1,11 @@
-def greet(name): #pass function with parameter
-    print(f'Hello {name}')
-    print('How are you ')
-    print('nice to meet you ')
+#def greet(name): #pass function with parameter
+#    print(f'Hello {name}')
+#   print('How are you ')
+#   print('nice to meet you ')
 
-greet('cynthia') #Adding an argument to be included in the functions output
+#greet('cynthia') #Adding an argument to be included in the functions output
+def greet_with(name="Matuu", location="Evans"):
+    print(f'Hello {name}')
+    print(f'What is it like in {location}?')
+
+greet_with( )
